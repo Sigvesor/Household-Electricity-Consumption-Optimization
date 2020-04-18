3571b5c21297f7c0da9858e91566f3135ebf2896
alfredo <alfredo@alf.dd>
2020-04-18 12:24:22 +0200

    working hourly variables

--- assignment3.py
+++ assignment3.py
@@ -30,7 +30,7 @@ class Optimisation:
         'lighting': [ # Lightning between 10:00-20:00
             1.50, 
             1.5/11, 
-            [i in list(range(10, 20+1)) for i in range(24)],
+            list(range(10, 20+1)),
             ], 
         'heating': [ # Household heating
             8.00,
@@ -63,12 +63,12 @@ class Optimisation:
         'dishes': [ # Dishwasher
             1.44, 
             .35, 
-            [i in list(range(4, 6+1)) + list(range(14, 18+1)) for i in range(24)],
+            list(range(4, 6+1)) + list(range(14, 18+1)),
             ], 
         'laundry': [ # Laundry machine
             1.94, 
             .5, 
-            [i in list(range(3, 7+1)) + list(range(14, 22+1)) for i in range(24)],
+            list(range(3, 7+1)) + list(range(14, 22+1)),
             ], 
         'dryer': [ # Clothes dryer
             2.50, 
@@ -77,8 +77,8 @@ class Optimisation:
             ],
         'ev': [ # Electric Vehicle
             9.90, 
-            9.9/13, # energy demanded devided by 13operating hours
-            [i in list(range(0, 6+1)) +  list(range(18, 23+1)) for i in range(24)],
+            .8, # energy demanded devided by 13operating hours
+            list(range(0, 6+1)) +  list(range(18, 23+1)),
             ], 
         # 'vg': 0.09, # Game console
         # 'phone': 0.04, # Cellphone charge (x3) - 5W*3h
@@ -97,7 +97,30 @@ class Optimisation:
             orient='index',
             columns=['e', 'p', 'ts'],
             )
+        self.apps['oh'] = self.apps['ts'].apply(lambda x: len(x))
         
+    def _split_app_matrix(self, tmp):
+        ts = {}
+        p_val = self._conc_app_matrix()
+        for idx, (key, row)  in enumerate(self.apps.iterrows()):
+            if idx != 0:
+                start = np.where(p_val[idx-1] != 0)[0][-1] + 1
+            else:
+                start = idx
+            ts[key] = [row['ts'], tmp[start:start + row['oh']]]
+        return ts
+            
+    def _conc_app_matrix(self):
+        apps = self.apps
+        n_variables = sum(apps['oh'])
+        tmp = np.zeros((len(apps), n_variables))
+        for idx, item in enumerate(apps['oh']):
+            if idx != 0:
+                start = np.where(tmp[idx-1] != 0)[0][-1] + 1
+            else:
+                start = idx
+            tmp[idx, start:start + item] = apps['p'].iloc[idx]
+        return tmp
 
 class Prob1(Optimisation):
     """Problem 1 - subclass
@@ -107,11 +130,8 @@ class Prob1(Optimisation):
             app_names=['dishes', 'laundry', 'ev'],
             )
         self.pricing_time = [i in list(range(17, 20+1)) for i in range(24)]
-        self.pricing = {
-            'peak': 1, # €/kWh
-            'base': .5, # €/kWh
-            }
-        self.set_time()
+        self.pricing = [1 if i else .5 for i in self.pricing_time]
+        # self.set_time()
         self.optimise()
         
     def set_time(self):
@@ -129,27 +149,31 @@ class Prob1(Optimisation):
         x: list of x values (true operating hours for every app for either base or peak timer)
             op_dish_p, op_laun_p, op_ev_p, op_dish_b, op_laun_b, op_ev_b
         """
-        c = array([x * self.pricing['peak'] for x in self.apps['p']] + [x *self.pricing['base'] for x in self.apps['p']])
-        # A_ub = np.eye(6) 
-        # b_ub = array([sum(x) for x in self.apps['ts_p']] + [sum(x) for x in self.apps['ts_b']])
-        # bounds = np.array([
-        #     np.zeros(6), 
-        #     np.concatenate(
-        #         ([sum(x) for x in self.apps['ts_p']], [sum(x) for x in self.apps['ts_b']])
-        #         )]
-        #     ).transpose().tolist()
-        bounds = tuple((0, sum(x)) for x in self.apps['ts_p']) + tuple((0, sum(x)) for x in self.apps['ts_b'])
-        A_eq = np.concatenate(([np.eye(3) * self.apps['p'].to_numpy()] * 2), axis=1)
+        k = []
+        for idx, row in self.apps.iterrows():
+            k = k + [self.pricing[i] * row['p'] for i in row['ts']]
+        c = array(k)
+        # c = array([x * self.pricing['peak'] for x in self.apps['p']] + [x *self.pricing['base'] for x in self.apps['p']])
+        bounds = tuple([(0, 1)] * len(c))
+        # bounds = tuple((0, sum(x)) for x in self.apps['ts_p']) + tuple((0, sum(x)) for x in self.apps['ts_b'])
+        # A_eq = np.zeros((len(self.apps), len(c)))
+        # for idx, item in enumerate(self.apps['oh']):
+        #     if idx != 0:
+        #         start = np.where(A_eq[idx-1] != 0)[0][-1] + 1
+        #     else:
+        #         start = idx
+        #     A_eq[idx, start:start + item] = self.apps['p'].iloc[idx]
+        A_eq = self._conc_app_matrix()
+        # A_eq = np.concatenate(([np.eye(3) * self.apps['p'].to_numpy()] * 2), axis=1)
         b_eq = array(self.apps['e'].tolist())
         res = linprog(
             c,
-            # A_ub=A_ub,
-            # b_ub=b_ub,
             A_eq=A_eq,
             b_eq=b_eq,
             bounds=bounds,
             method='simplex',
             )
+        res_var = self._split_app_matrix(res.x)        
         print('A_eq:')
         print(A_eq)
         print('b_eq:')
@@ -172,5 +196,5 @@ class Prob2(Optimisation):
         
 if __name__ == "__main__":
     obj = Prob1()
-    obj = Prob2()
+    # obj = Prob2()
     
