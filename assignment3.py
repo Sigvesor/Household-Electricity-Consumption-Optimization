e63d52e3b501eeab6204fce2e3989a8a20945c87
alfredo <alfredo@alf.dd>
2020-04-19 18:15:16 +0200

    working optimisation

--- assignment3.py
+++ assignment3.py
@@ -11,6 +11,7 @@ from numpy import array
 import pandas as pd
 import scipy as sp
 from scipy.optimize import linprog
+import matplotlib.pyplot as plt
 
 
 class Optimisation:
@@ -34,28 +35,28 @@ class Optimisation:
             ], 
         'heating': [ # Household heating
             8.00,
-            None,
-            [],
+            8/24,
+            list(range(10, 20+1)),
             ],
         'fridge': [ # fridge/freezer + small freezer
             3.00, 
-            None,
-            [],
+            3/24,
+            list(range(24)),
             ],
         'stove': [ # Cooking stove
             3.90, 
-            None,
-            [],
+            2.40,
+            list(range(5, 7+1)) + list(range(14, 22+1)),
             ],
         'tv': [ # Medium size TV
-            0.40, 
-            None,
-            [],
+            .15, 
+            .03,
+            list(range(4, 7+1)) + list(range(14, 22+1)),
             ],
         'computer': [ # 2 computers
-            1.20, 
-            None,
-            [],
+            .60,
+            .12,
+            list(range(4, 7+1)) + list(range(14, 22+1)),
             ],
         }
     # Shiftable appliances [kWh/day]
@@ -72,27 +73,61 @@ class Optimisation:
             ], 
         'dryer': [ # Clothes dryer
             2.50, 
-            None,
-            [],
+            3,
+            list(range(3, 7+1)) + list(range(14, 22+1)),
             ],
         'ev': [ # Electric Vehicle
             9.90, 
-            .8, # energy demanded devided by 13operating hours
+            3, # energy demanded devided by 13operating hours
             list(range(0, 6+1)) +  list(range(18, 23+1)),
             ], 
-        # 'vg': 0.09, # Game console
-        # 'phone': 0.04, # Cellphone charge (x3) - 5W*3h
-        # 'coffee': 0.28, # Coffee-maker - 800W*0.17h*2
-        # 'vacuum': 0.23, # Vacuum cleaner - 1400W*0.17h
-        # 'microwave': 0.30, # 1200W*0.25h
-        # 'wifi': 0.14, # Router - 6W*24h
-        # 'ac': 3.00, # Air-condition - 1kW*3h
-        # 'waterheater': 12.0, # 4kW*3h
+        'gc': [ # Game console
+            0.27, 
+            0.09,
+            list(range(18, 23+1)),
+            ], 
+        'phone': [ # Cellphone charge (x3)
+            0.05, 
+            0.015,
+            list(range(0, 5+1)) + list(range(22, 23+1)),
+            ],
+        'coffee': [ # Coffee maker, 2 times/day
+            0.53, 
+            0.80,
+            list(range(5, 9+1)) + list(range(14, 18+1)),
+            ], 
+        'vacuum': [ # Vacuum cleaner
+            0.23, 
+            1.40,
+            list(range(14, 22+1)),
+            ], 
+        'microwave': [ # 
+            0.30, 
+            1.20,
+            list(range(6, 8+1)) + list(range(14, 22+1)),
+            ], 
+        # 'wifi': [ # Router
+        #     0.14, 
+        #     0.006,
+        #     list(range(24)),
+        #     ], 
+        'ac': [ # Air-condition
+            3.00, 
+            1.00,
+            list(range(24)),
+            ], 
+        'waterheater': [ # 
+            12.0, 
+            4.00,
+            list(range(24)),
+            ], 
         }
-    apps = {**non_shift_app, **shift_app}
+    # apps = {**non_shift_app, **shift_app}
+    apps = shift_app
         
     def __init__(self, app_names=None, pricing=None):
         self.nr_of_hours = 24
+        self.power_max = 10
         self.apps = pd.DataFrame.from_dict(
             self.apps if not app_names else {key: self.apps[key] for key in app_names}, 
             orient='index',
@@ -100,7 +135,17 @@ class Optimisation:
             )
         self.apps['oh'] = self.apps['ts'].apply(lambda x: len(x))
         self.time_matrix = self._create_time_matrix()
-        
+        if pricing:
+            # self.pricing = self._rtp_noise(
+            #     pd.read_csv('nordpool_20200303.csv', decimal=',')[pricing][0:self.nr_of_hours].to_numpy() / 1000 # €/kWh
+            #     )
+            self.pricing = pd.read_csv('nordpool_20200303.csv', decimal=',')[pricing][0:self.nr_of_hours].to_numpy() / 1000 # €/kWh
+        else:
+            pricing_time = [i in list(range(17, 20+1)) for i in range(self.nr_of_hours)]
+            self.pricing = [.1 if i else .05 for i in pricing_time] # €/kWh
+        self.raw_result = []
+        self.result = []
+            
     def _create_time_matrix(self):
         tmp = np.zeros((self.nr_of_hours, len(self.apps)))
         for idx1, (key, row) in enumerate(self.apps.iterrows()):
@@ -109,38 +154,63 @@ class Optimisation:
         tmp = pd.DataFrame(tmp, columns=self.apps.index)
         return tmp
     
-    def _create_var_matrix(self):
-        pass
-        
-    # def _split_app_matrix(self, tmp):
-    #     ts = {}
-    #     p_val = self._conc_app_matrix()
-    #     for idx, (key, row)  in enumerate(self.apps.iterrows()):
-    #         if idx != 0:
-    #             start = np.where(p_val[idx-1] != 0)[0][-1] + 1
-    #         else:
-    #             start = idx
-    #         ts[key] = array([row['ts'], tmp[start:start + row['oh']].tolist()]).transpose()
-    #     return ts
+    def _resolve_res_matrix(self, res):
+        if not res.success:
+            print('optimisation not successfull')
+            pass
+        x = res.x
+        res_time_wise = x.reshape(self.nr_of_hours, int(x.size / self.nr_of_hours), order='F')
+        return res_time_wise
             
-    # def _conc_app_matrix(self):
-    #     apps = self.apps
-    #     n_variables = sum(apps['oh'])
-    #     tmp = np.zeros((len(apps), n_variables))
-    #     for idx, item in enumerate(apps['oh']):
-    #         if idx != 0:
-    #             start = np.where(tmp[idx-1] != 0)[0][-1] + 1
-    #         else:
-    #             start = idx
-    #         tmp[idx, start:start + item] = 1
-    #     return tmp.transpose()
+    def _conc_aeq_matrix(self, matrix):
+        shape = matrix.shape
+        n_col = shape[1]
+        n_row = shape[0]
+        tmp = np.array([])
+        for i in range(n_col):
+            if i == 0:
+                tmp = np.eye(n_row) * matrix.T[i]
+            else:
+                tmp = np.concatenate((tmp, np.eye(n_row) * matrix.T[i]), axis=1)
+        return tmp
     
-    # def _conc_upper_bounds_matrix(self):
-    #     tmp = np.zeros((24, len(self.apps)))
-    #     for idx1, (key, row) in enumerate(self.apps.iterrows()):
-    #         for idx2 in row['ts']:
-    #             tmp[idx2, idx1] = 1
-    #     return tmp
+    def _conc_aub_matrix(self, matrix):
+        shape = matrix.shape
+        n_col = shape[1]
+        n_row = shape[0]
+        tmp = np.zeros((n_row, n_col * n_row))
+        for idx, row in enumerate(matrix):
+            tmp[idx][idx * n_col:(1 + idx) * n_col] = row
+        return tmp
+        
+    def _rtp_noise(self, pricing):
+        noise = np.random.randint(-100, 100, len(pricing)) / 100
+        pricing += noise * pricing * .1
+        return pricing
+    
+    def execute(self):
+        raw_result = self.optimise()
+        self.raw_result = raw_result()
+        power, pricing = self.resolve_result(raw_result)
+        self.result = {
+            'power': power,
+            'pricing': pricing,
+            }
+        self.plot_time_wise(power)
+        
+    def resolve_result(self, raw_result):
+        res_time_wise = self._resolve_res_matrix(raw_result)
+        res_power = res_time_wise * self.apps['p'].to_numpy()
+        res_pricing = res_time_wise.T * self.pricing
+        power = pd.DataFrame(
+            res_power,
+            columns=self.apps.index,
+            )
+        pricing = pd.DataFrame(
+            res_pricing.T,
+            columns=self.apps.index,
+            )
+        return power, pricing
     
     def optimise(self):
         """Run Optimisations
@@ -151,37 +221,43 @@ class Optimisation:
         x: list of x values (true operating hours for every app for either base or peak timer)
             op_dish_p, op_laun_p, op_ev_p, op_dish_b, op_laun_b, op_ev_b
         """
-        # k = []
-        # for idx, row in self.apps.iterrows():
-        #     k = k + [self.pricing[i] * row['p'] for i in row['ts']]
-        # k = app_matrix * self.pricing
-        # c = array(k)
-        power_matrix = self.time_matrix.to_numpy() * self.apps['p'].to_numpy()
+        time_matrix = self.time_matrix.to_numpy()
+        power_matrix = time_matrix * self.apps['p'].to_numpy()
         price_matrix = power_matrix.T * self.pricing
-        A_ub = power_matrix
-        b_ub = np.ones((self.nr_of_hours, 1)) * 20
-        A_eq = power_matrix.transpose()
+        A_ub = self._conc_aeq_matrix(power_matrix)
+        b_ub = np.ones(self.nr_of_hours) *  self.power_max
+        A_eq = self._conc_aub_matrix(power_matrix.T)
         b_eq = self.apps['e'].to_numpy()
-        # A_eq = app_matrix * self.apps['p'].to_numpy()
-        # b_eq = self.apps['e'].to_numpy().transpose()
-        c = price_matrix.reshape(self.time_matrix.size)
-        bounds = tuple([(0, 1)] * len(c))
-        res = linprog(
+        c = price_matrix.ravel()
+        bounds = tuple([(0, x) for x in time_matrix.T.ravel()])
+        raw_result = linprog(
             c,
             A_eq=A_eq,
             b_eq=b_eq,
             A_ub=A_ub,
             b_ub=b_ub,
             bounds=bounds,
-            method='simplex',
+            method='revised simplex',
             options={
-                'disp':True, 'maxiter':5000
+                'disp':True, 
+                'maxiter':5000,
                 },
             )
-        res_var = self._split_app_matrix(res.x)        
-        # print('cost: ' + str(res.fun) + ' €/d')
+        # print(raw_result.slack)
+        return raw_result
+        
+    def plot_time_wise(self, data):
+        fig = plt.figure(figsize=(18,6))
+        ax = fig.gca()
+        data.plot(
+            ax=ax,
+            drawstyle="steps-post",
+            linewidth=2,
+            legend=False,
+            )
+        ax.set_xticks(range(self.nr_of_hours))
 
-    
+
 class Prob1_simple(Optimisation):
     """Problem 1 - subclass
     """
@@ -229,21 +305,6 @@ class Prob1_simple(Optimisation):
             bounds=bounds,
             method='simplex',
             )
-        print('cost: ' + str(res.fun) + ' €/d')
-        # print('A_eq:')
-        # print(A_eq)
-        # print('b_eq:')
-        # print(b_eq)
-        # print('A_ub:')
-        # print(A_ub)
-        # print('b_ub:')
-        # print(b_ub)
-        # print('bounds:')
-        # print(bounds)
-        # print('c:')
-        # print(c)
-        # print('results:')
-        # print(res)
         
 
 class Prob1(Optimisation):
@@ -253,40 +314,36 @@ class Prob1(Optimisation):
         super().__init__(
             app_names=['dishes', 'laundry', 'ev'],
             )
-        pricing_time = [i in list(range(17, 20+1)) for i in range(self.nr_of_hours)]
-        self.pricing = [.1 if i else .05 for i in pricing_time] # €/kWh
-        self.optimise()
-    
-        
+        self.execute()
+
+
 class Prob2(Optimisation):
-    def __init__(self, data_name='Krsand'):
+    def __init__(self, **kwargs):
         super().__init__(
-            app_names=['dishes', 'laundry', 'ev'],
+            # app_names=['dishes', 'laundry', 'ev'],
+            pricing=kwargs.get('data_name', 'Krsand'),
             )
-        self.pricing = pd.read_csv('nordpool_20200303.csv', decimal=',')[data_name][0:self.nr_of_hours].to_numpy() / 1000 # €/kWh
-        self.rtp_noise()
-        self.optimise()
-    
-    def rtp_noise(self):
-        noise = np.random.randint(-100, 100, len(self.pricing)) / 100
-        self.pricing += noise * self.pricing * .1
+        self.execute()
+
         
 class Prob3(Optimisation):
-    def __init__(self, data_name='Krsand'):
+    def __init__(self, **kwargs):
         super().__init__(
-            app_names=['dishes', 'laundry', 'ev'],
+            # app_names=['dishes', 'laundry', 'ev'],
+            pricing=kwargs.get('data_name', 'Krsand'),
             )
         n_housholds = 30
+        self.power_max *= n_housholds
         self.apps = pd.concat([self.apps]*n_housholds, keys=range(n_housholds))
-        self.pricing = pd.read_csv('nordpool_20200303.csv', decimal=',')[data_name][0:self.nr_of_hours] / 1000 # €/kWh
-        self.optimise()
+        self.time_matrix = self._create_time_matrix()
+        self.execute()
         
         
         
 if __name__ == "__main__":
     # obj = Prob1_simple()
     # obj = Prob1()
-    obj = Prob2()
+    # obj = Prob2()
     # obj = Prob2(data_name='Ger')
-    # obj = Prob3()
+    obj = Prob3()
     
